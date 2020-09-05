e6384c09c6307530725f7c4d4ccdce5e17f074d2
sahil <sahil.sachdeva@83incs.com>
2020-09-05 20:38:23 +0530

    code added

---
+++ src/com/company/Array/SumCode.java
@@ -0,0 +1,25 @@
+package com.company.Array;
+
+public class SumCode {
+    static int i; static int j=i+1;
+
+
+    public static void method(int[] array,int length,int selectNum) {
+
+        for (i=0;i< array.length-1;i++){
+
+            if (j != length && (array[i] + array[j]) ==selectNum) {
+
+                System.out.println(array[i] + " and " + array[j]);
+                j++;
+                method(array, length,selectNum);
+
+
+            }
+        }
+    }
+    public static void main(String[] args) {
+        int[] array = {1,2,3,4};
+        method(array, array.length-1,7);
+    }
+}
--- src/com/company/InterviewQues/GoogleQuesSumOfValue.java
+++ src/com/company/InterviewQues/GoogleQuesSumOfValue.java
@@ -1,32 +1,32 @@
 package com.company.InterviewQues;
 
+import java.util.Arrays;
+
 public class GoogleQuesSumOfValue {
 
     public static void main(String[] args) {
 
-        int[] arr = {3,2,3,14};
+        int[] arr = {3, 2, 3, 14};
 
-        for(int i: twoSum(arr,6 )){
-            System.out.println(i);
-        }
+        System.out.println(Arrays.toString(twoSum(arr, 6)));
+
     }
+
     public static int[] twoSum(int[] nums, int target) {
 
         int low = 0;
-        int high = nums.length-1;
+        int high = nums.length - 1;
 
-        while(low<high){
-
-
-            int sum =nums[low]+nums[high];
-            if(sum==target){
-                int[] arr= {low,high};
+        while (low < high) {
+            int sum = nums[low] + nums[high];
+            if (sum == target) {
+                int[] arr = {low, high};
                 return arr;
             }
-            high=high-1;
-            if(low ==high){
-                low = low+1;
-                high = nums.length-1;
+            high = high - 1;
+            if (low == high) {
+                low = low + 1;
+                high = nums.length - 1;
             }
 
         }
