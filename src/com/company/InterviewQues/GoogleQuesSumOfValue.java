0a4e51b4edb9547bfbd11edf22e8c2448107fb58
sahil <sahil.sachdeva@83incs.com>
2020-09-05 13:50:49 +0530

    sum of two number ques solved

--- src/com/company/InterviewQues/GoogleQuesSumOfValue.java
+++ src/com/company/InterviewQues/GoogleQuesSumOfValue.java
@@ -4,16 +4,34 @@ public class GoogleQuesSumOfValue {
 
     public static void main(String[] args) {
 
+        int[] arr = {3,2,3,14};
 
-        System.out.println("hello world ..!!!");
-        System.out.println("hello from new branch" +
-                " ..!!!");
+        for(int i: twoSum(arr,6 )){
+            System.out.println(i);
+        }
+    }
+    public static int[] twoSum(int[] nums, int target) {
+
+        int low = 0;
+        int high = nums.length-1;
+
+        while(low<high){
 
 
+            int sum =nums[low]+nums[high];
+            if(sum==target){
+                int[] arr= {low,high};
+                return arr;
+            }
+            high=high-1;
+            if(low ==high){
+                low = low+1;
+                high = nums.length-1;
+            }
 
+        }
+
+        return new int[2];
 
     }
-
-
-
 }
