7c346a4f74ab2c5566caeaa8be41d41316e87b99
Harpreet <Codeobar@gmail.com>
2020-10-23 01:35:52 +0530

    Rotate left array on given target

--- src/com/company/Array/RotateLeft.java
+++ src/com/company/Array/RotateLeft.java
@@ -4,22 +4,27 @@ import java.util.Arrays;
 
 public class RotateLeft{
 
-    public static int[] getRotateArr(int[] array,int times,int target){
-        if (times!=target){
-            times++;
-            int lastElement = array[array.length-1];
-            for (int i=array.length-1;i>0;i--) {
-                array[i] = array[i-1];
-            }
-            array[0] = lastElement;
-            getRotateArr(array,times,target);
-        }
-        return array;
+    public static void getRotateArr(int[] array,int target){
+       int start = 0;
+       int end = array.length-target;
+       int loop = target-1;
+       int[] arr = new int[array.length];
+
+       for (int i=0;i<array.length;i++) {
+           if (i <= loop){
+               arr[end] = array[i];
+               end++;
+           }
+           if (i > loop) {
+               arr[start] = array[i];
+               start++;
+           }
+       }
+        System.out.println(Arrays.toString(arr));
     }
 
     public static void main(String[] args) {
-        int[] array = {1,2,3,4,5};
-        int[] arr = getRotateArr(array,0,3);
-        System.out.println(Arrays.toString(arr));
+        int[] array = {1,2,3,4,5,6,7};
+        getRotateArr(array,4);
     }
 }
