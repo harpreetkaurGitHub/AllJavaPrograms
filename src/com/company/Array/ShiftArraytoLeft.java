fad9086f7184064ee40c6cad59dc5c4f21df4e19
Harpreet <Codeobar@gmail.com>
2020-10-24 21:08:57 +0530

    Shift array to left

--- src/com/company/Array/ShiftArraytoLeft.java
+++ src/com/company/Array/ShiftArraytoLeft.java
@@ -5,20 +5,17 @@ import java.util.Arrays;
 public class ShiftArraytoLeft {
 
     public static void method(int[] array,int target){
-        int temp=0; int zero =0;
+        int zero =0;
 
-        for (int i=0;i<array.length-1;i++) {
-            if (array[i] != target) {
-                array[temp] = array[i];
-                temp++;
-            }
-            array[array.length-1] = zero;
+        for (int i=target;i<array.length-1;i++) {
+            array[i] = array[i+1];
         }
+        array[array.length-1] = zero;
         System.out.println(Arrays.toString(array));
     }
 
     public static void main(String[] args) {
-        int[] array = {1,3,5,2,8,7,20,4,7,6,4,0,66,54,27,-9,1,-2,4};
-        method(array,20);
+        int[] array = {1,3,5,2,8,7,20,4};
+        method(array,6);
     }
 }
